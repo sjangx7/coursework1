f91be781504139eb164349eb333c49224b4ecece
“sjangx7” <sjangx7@gmail.com>
2024-11-14 22:05:58 +0000

    finally

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,21 +1,7 @@
-public class Dec2Hex {
+import java.util.Scanner;
 
-    // Method for converting decimal to hexadecimal
-    public String convertToHex(int num) {
-        if (num == 0) return "0";
-
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        int rem;
-        String hexadecimal = "";
-
-        // Conversion process
-        while (num != 0) {
-            rem = num % 16;
-            hexadecimal = ch[rem] + hexadecimal;
-            num = num / 16;
-        }
-        return hexadecimal;
-    }
+class Dec2Hex {
+    public static int Arg1;
 
     public static void main(String args[]) {
         // Check if an argument is provided
@@ -26,13 +12,28 @@ public class Dec2Hex {
 
         try {
             // Try to parse the input as an integer
-            int num = Integer.parseInt(args[0]);
-            Dec2Hex dec2Hex = new Dec2Hex();
-            String hexadecimal = dec2Hex.convertToHex(num);
-            System.out.println("Hexadecimal representation is: " + hexadecimal);
+            Arg1 = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
             // Catch non-integer inputs and display an error message
             System.out.println("Error: Invalid input. Please enter a valid integer.");
+            return;
         }
+
+        // Array of hexadecimal characters
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal = "";
+
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+        // Conversion process
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
+        }
+
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
     }
 }
