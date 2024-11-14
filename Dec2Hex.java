c0b08ec0c9ab19b9508514760d139fbff37b8940
“sjangx7” <sjangx7@gmail.com>
2024-11-14 21:49:33 +0000

    changes

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,39 +1,24 @@
-public class Dec2Hex {
+import java.util.Scanner;
 
-    // Method for converting decimal to hexadecimal
-    public String convertToHex(int num) {
-        if (num == 0) return "0";
+class Dec2Hex
+{
+public static int Arg1;
+    public static void main(String args[])    {
+        Arg1 = Integer.parseInt(args[0]);
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal="";
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        int rem;
-        String hexadecimal = "";
-
-        // Conversion process
-        while (num != 0) {
-            rem = num % 16;
-            hexadecimal = ch[rem] + hexadecimal;
-            num = num / 16;
-        }
-        return hexadecimal;
-    }
-
-    public static void main(String args[]) {
-        // Check if an argument is provided
-        if (args.length == 0) {
-            System.out.println("Error: No input provided. Please enter an integer value.");
-            return;
+        while(num != 0)
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
         }
 
-        try {
-            // Try to parse the input as an integer
-            int num = Integer.parseInt(args[0]);
-            Dec2Hex dec2Hex = new Dec2Hex();
-            String hexadecimal = dec2Hex.convertToHex(num);
-            System.out.println("Hexadecimal representation is: " + hexadecimal);
-        } catch (NumberFormatException e) {
-            // Catch non-integer inputs and display an error message
-            System.out.println("Error: Invalid input. Please enter a valid integer.");
-        }
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
+
     }
 }
-
